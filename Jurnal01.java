b701b70502b804c84c719a15712bc63dcc2d7329
Abraham <abrahamputrabs@gmail.com>
2023-09-27 22:57:48 +0700

    initial commit "println Nama, NIM, Kelas"

--- Jurnal01.java
+++ Jurnal01.java
@@ -1,5 +1,7 @@
-public class Jurnal01{
-    public static void main(String[] args){
-        
+public class Jurnal01 {
+    public static void main(String[] args) {
+        System.out.println("Nama: Abraham Putra Berman Simbolon");
+        System.out.println("NIM: 607062300058");
+        System.out.println("Kelas: D3RPLA-47-01");
     }
 }
